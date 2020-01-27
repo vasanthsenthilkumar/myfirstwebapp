5ef08c83f6d8a67b8840ad1194225c96c4dddbcd
vasanthsenthilkumar <vasanthedison@gmail.com>
2020-01-27 14:16:42 +0530

    myfirstwebapp

---
+++ myfirstwebapp/src/main/java/com/fernbird/Alien.java
@@ -0,0 +1,32 @@
+package com.fernbird;
+
+public class Alien {
+	public String aname;
+	public String lang;
+	public int aid;
+
+	public String getAname() {
+		return aname;
+	}
+
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+
+	public String getLang() {
+		return lang;
+	}
+
+	public void setLang(String lang) {
+		this.lang = lang;
+	}
+
+	public int getAid() {
+		return aid;
+	}
+
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+
+}
---
+++ myfirstwebapp/src/main/java/com/fernbird/Homecontroller.java
@@ -0,0 +1,25 @@
+package com.fernbird;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class Homecontroller {
+	
+ @RequestMapping("home")
+	
+ public ModelAndView home(Alien alien)
+ {
+	 ModelAndView mv = new ModelAndView();
+        mv.addObject("obj",alien);
+       mv.setViewName("home");
+       return mv;
+ }
+ 
+}
+
+
+        
+
